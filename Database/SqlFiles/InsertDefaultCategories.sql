21c9bcd04c0f59cbe840d9dbadf3c3a2665e3676
kristiyan <kristiyann03@gmail.com>
2022-07-29 15:03:13 +0300

    Insert Default Categories SQL Script

---
+++ Database/SqlFiles/InsertDefaultCategories.sql
@@ -0,0 +1,23 @@
+INSERT INTO Tags
+(Id, Name)
+VALUES
+('dcdf398c-f477-499f-a5e7-0fa71b9e9500', 'Pasta'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9501', 'Noodles'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9502', 'Pizza'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9503', 'Soup'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9504', 'Salad'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9505', 'Baking'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9506', 'Sandwich'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9507', 'Dessert'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9508', 'Snack'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9509', 'Eggs'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9510', 'Smoothies'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9511', 'Vegetarian'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9512', 'Chicken'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9513', 'Beef'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9514', 'Pork'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9515', 'Fish'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9516', 'Asian'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9517', 'Italian'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9518', 'Bulgarian'),
+('dcdf398c-f477-499f-a5e7-0fa71b9e9519', 'Mexican')
